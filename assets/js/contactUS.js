a8aa74303a7a74e155e4deef7e019baece9ab17b
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2018-11-07 15:55:20 +0100

    v1.0 done

--- assets/js/contactUS.js
+++ assets/js/contactUS.js
@@ -1,18 +1,33 @@
 const ButtonMessage = document.getElementById("ButtonMessage");
 const formulaire = document.getElementById("formulaire");
 const buttonSendContactUS = document.getElementById("buttonSendContactUS");
+const CancelMessage = document.getElementById("CancelMessage");
 //stock dans des constantes les 4 formulaires
 const FirstName = document.getElementById("FirstName");
 const LastName = document.getElementById("LastName");
 const email = document.getElementById("email");
-const Subject =document.getElementById("Subject");
+const Subject = document.getElementById("Subject");
+const Message = document.getElementById("Message");
 
 ButtonMessage.addEventListener("click", function(){
-    console.log("yes");
     formulaire.style.display = "block"; //affiche le formulaire
 });
-//formulaire.style.display = "block"; 
 
-buttonSendContactUS.addEventListener("click", function(){
-    alert("Your first name is : "+FirstName.value);
+CancelMessage.addEventListener("click", function(){
+    formulaire.style.display = "none"; //cache le formulaire
 });
+
+/*
+buttonSendContactUS.addEventListener("click", function(){
+    alert("Your first name is : "+FirstName.value
+    +"\nYour Last name is : "+LastName.value+"\nYour Email is : "+email.value+"\nYour Subject name is : "+Subject.value);
+});
+*/
+
+/*
+buttonSendContactUS.addEventListener("click", function(){
+    if (FirstName.value != null && LastName.value != null && email.value != null && Subject.value != null){
+        alert("Merci pour ton message, "+FirstName.value);
+    }
+});
+*/
