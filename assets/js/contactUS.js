e3c39b2643429da9e25fe37b81e91f6ed826c405
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2018-11-07 15:18:05 +0100

    pushing

--- assets/js/contactUS.js
+++ assets/js/contactUS.js
@@ -1,9 +1,18 @@
 const ButtonMessage = document.getElementById("ButtonMessage");
 const formulaire = document.getElementById("formulaire");
 const buttonSendContactUS = document.getElementById("buttonSendContactUS");
+//stock dans des constantes les 4 formulaires
+const FirstName = document.getElementById("FirstName");
+const LastName = document.getElementById("LastName");
+const email = document.getElementById("email");
+const Subject =document.getElementById("Subject");
 
 ButtonMessage.addEventListener("click", function(){
     console.log("yes");
     formulaire.style.display = "block"; //affiche le formulaire
 });
 //formulaire.style.display = "block"; 
+
+buttonSendContactUS.addEventListener("click", function(){
+    alert("Your first name is : "+FirstName.value);
+});
