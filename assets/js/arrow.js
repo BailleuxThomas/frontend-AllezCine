af193fca4f2cd448d240c53ff1ffea56ec7608ff
BailleuxThomas <thomasbailleux2@gmail.com>
2018-11-13 10:25:05 +0100

    mise à jours thomas

--- assets/js/arrow.js
+++ assets/js/arrow.js
@@ -17,7 +17,7 @@ jQuery(function(){
   $(function () {
   $(window).scroll(function () { //Fonction appelée quand on descend la page
   if ($(this).scrollTop() > 200 ) {  // Quand on est à 200pixels du haut de page,
-  $('#arrow').css('right','10px'); // Replace à 10pixels de la droite l'image
+  $('#arrow').css('right','15px'); // Replace à 10pixels de la droite l'image
   } else { 
   $('#arrow').removeAttr( 'style' ); // Enlève les attributs CSS affectés par javascript
   }
---
+++ assets/js/navbar.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("navbar").style.top = "0px";
+    document.getElementById("navbar").style.backgroundColor = "rgba(0,0,0,0.5)";
+  } else if (document.body.scrollTop < 100 || document.documentElement.scrollTop < 100){
+    document.getElementById("navbar").style.top = "0px";
+    document.getElementById("navbar").style.backgroundColor = "";
+    document.getElementById("navbar").style.margin = "0";
+    document.getElementById("navbar").style.padding = "0";
+  } else  {
+    document.getElementById("navbar").style.top = "0px";
+  }
+}
