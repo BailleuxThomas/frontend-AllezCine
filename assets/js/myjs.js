60a8f02c54f2119ec688e517e4d9ebe68fbb2c54
bouzouitadavid <davidbouzouita@gmail.com>
2018-11-13 10:57:16 +0100

    correction js

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -14,8 +14,8 @@ function film(title, src, alt, date, genre, price, productor, actor, story, ytsr
 }
 array = []
 array.push(
-	new film("Batman", "batmanmovie-2017-comedie", "batman", "13 aout 2008", "comedie", 20, "George Clooney, Christopher Nolan", "Dans cette nouvelle aventure, Batman aura bien besoin de son audacieux partenaire Robin, juché sur sa fringante moto turbo. En effet, le glacial M. Freeze fait régner une vague de froid polaire sur Gotham City avec la complicité de la belle et vénéneuse Poison Ivy, au baiser mortel et aux formes sinueuses, qui rêve de soumettre le monde au pouvoir des femmes-fleurs.", "https://www.youtube.com/embed/f64h6dc2rqM"),
-	new film("Hostel", "hostel-2005-thriller", "hostel", "1 mars 2006", "thriller", 18, "Eli Roth", "Deux étudiants américains, Paxton et Josh, ont décidé de découvrir l'Europe avec un maximum d'aventures et de sensations fortes. Avec Oli, un Islandais qu'ils ont rencontré en chemin, ils se retrouvent dans une petite ville de Slovaquie dans ce qu'on leur a décrit comme le nirvana des vacances de débauche : une propriété très spéciale, pleine de filles aussi belles que faciles... Natalya et Svetlana sont effectivement très cools... un peu trop, même. Paxton et Josh vont vite se rendre compte qu'ils sont tombés dans un piège. Ce voyage-là va les conduire au bout de l'horreur... ", "https://www.youtube.com/embed/cVBdQiUHhZI"),
+	new film("Batman", "batmanmovie-2017-comedie", "batman", "13 aout 2008", "comedie", 20, "George Clooney, Christopher Nolan", "Eli Roth", "Dans cette nouvelle aventure, Batman aura bien besoin de son audacieux partenaire Robin, juché sur sa fringante moto turbo. En effet, le glacial M. Freeze fait régner une vague de froid polaire sur Gotham City avec la complicité de la belle et vénéneuse Poison Ivy, au baiser mortel et aux formes sinueuses, qui rêve de soumettre le monde au pouvoir des femmes-fleurs.", "https://www.youtube.com/embed/f64h6dc2rqM"),
+	new film("Hostel", "hostel-2005-thriller", "hostel", "1 mars 2006", "thriller", 18, "Ellen Page", "Eli Roth", "Deux étudiants américains, Paxton et Josh, ont décidé de découvrir l'Europe avec un maximum d'aventures et de sensations fortes. Avec Oli, un Islandais qu'ils ont rencontré en chemin, ils se retrouvent dans une petite ville de Slovaquie dans ce qu'on leur a décrit comme le nirvana des vacances de débauche : une propriété très spéciale, pleine de filles aussi belles que faciles... Natalya et Svetlana sont effectivement très cools... un peu trop, même. Paxton et Josh vont vite se rendre compte qu'ils sont tombés dans un piège. Ce voyage-là va les conduire au bout de l'horreur... ", "https://www.youtube.com/embed/cVBdQiUHhZI"),
 	new film("Inception", "inception-2010-scifi", "inception", "2010", "scifi", 19, "Leonardo DiCaprio", "Marion Cotillard, Ellen Page", "Dom Cobb est un voleur expérimenté – le meilleur qui soit dans l’art périlleux de l’extraction : sa spécialité consiste à s’approprier les secrets les plus précieux d’un individu, enfouis au plus profond de son subconscient, pendant qu’il rêve et que son esprit est particulièrement vulnérable. Très recherché pour ses talents dans l’univers trouble de l’espionnage industriel, Cobb est aussi devenu un fugitif traqué dans le monde entier qui a perdu tout ce qui lui est cher. Mais une ultime mission pourrait lui permettre de retrouver sa vie d’avant – à condition qu’il puisse accomplir l’impossibles", "https://www.youtube.com/embed/cVBdQiUHhZI"),
 	new film("Intouchables", "intouchables-2011-comedie", "intouchables", "2011", "comedie", 19, "François Cluzet", "Omar Sy", "A la suite d’un accident de parapente, Philippe, riche aristocrate, engage comme aide à domicile Driss, un jeune de banlieue tout juste sorti de prison. Bref la personne la moins adaptée pour le job. Ensemble ils vont faire cohabiter Vivaldi et Earth Wind and Fire, le verbe et la vanne, les costumes et les bas de survêtement... Deux univers vont se télescoper, s’apprivoiser, pour donner naissance à une amitié aussi dingue, drôle et forte qu’inattendue, une relation unique qui fera des étincelles et qui les rendra... Intouchables.", "https://www.youtube.com/embed/EsaX5kltRcA"),
 	new film("Le patient anglais", "lepatientanglais-1996-dramatique", "lepatientanglais", "1996", "dramatique", 15, "Ralph Fiennes", "Juliette Binoche", "En 1945, quatre personnes dechirées par la guerre se réfugient dans un monastère abandonné de Toscane pour se guérir de leurs maux. Peu à peu le passé va resurgir et la présence d'un homme mystérieux et méconnaissable, le patient anglais, va bouleverser le destin de ceux qui croisent son chemin.", "https://www.youtube.com/embed/xImomcZczRY"),
@@ -29,7 +29,6 @@ array.push(
 	new film("What We Do In The Shadows", "WhatWeDoInTheShadows-2014-comedie", "WhatWeDoInTheShadows", "2014", "comedie", 16, "Avec Taika Waititi", "Jemaine Clement", "Comment fait-on quand on est vampires depuis des siècles et qu’on doit discrètement vivre en coloc en 2015 dans la banlieue de Limoges ? C’est ce que nous propose de découvrir une équipe de documentaire, en partageant l’intimité d’une bande de potes suceurs de sang ! Geoffroy, Miguel, Aymeric et Bernard nous ouvrent les portes de leur cœur et celle de leur quotidien un tout petit peu complexe.", "https://www.youtube.com/embed/IAZEWtyhpes"),
 	new film("Zoolander", "zoolander-2001-comedie", "zoolander", "2001", "comedie", 17, "Ben Stiller", "Owen Wilson", "Derek Zoolander, un célèbre mannequin, est à l'apogée de sa carrière. Couronné trois fois de suite Top model de l'année, il est sur le point de recevoir son quatrième trophée lorsque ce titre lui est ravi par un jeune et ambitieux challenger : Hansel. Effondrée, l'idole des podiums décide d'abandonner l'univers futile de la mode pour se ressourcer au sein de sa famille en Pennsylvanie. C'est alors que Maury Ballstein, son agent, et Jacobim Mugatu, un styliste déjanté, proposent à Zoolander de devenir la star de la nouvelle ligne de vêtements Derelicte. Zoolander accepte, mais il est loin de s'imaginer que le diabolique Mugatu, secondé de la troublante Katinka, s'apprête à lui effectuer un lavage de cerveau. Ainsi lobotomisé, Derek pourrait exécuter plus facilement ses ordres, à savoir assassiner le président de la Malaisie.", "https://www.youtube.com/embed/SNixRYY7HBY"),
 );
-
 console.log(array)
 valTaille = "affiche col-md-2 col-5";
 // array = tableau sur base constructeur film
@@ -49,20 +48,10 @@ function selectCat(cat) {
 	}
 }
 // function pour afficher plus de résultat
-
-function affReduce() {
-	document.querySelector(".featuredMovies>.movieContainer").innerHTML = ""
-	for (let i = 0; i < 6; i++) {
-		element = array[i]
-		document.querySelector(".featuredMovies>.movieContainer").innerHTML += afficheMaker(element, valTaille)
-	}
-	document.querySelector(".featuredMovies>div>.more-movie").innerHTML = "PLUS DE FILMS"
-	document.querySelector(".featuredMovies>div>.more-movie").setAttribute("onClick", "affMore()")
-}
 function affMore() {
 	for (let i = 0; i < 6; i++) {
 		newList = []
-		val = document.querySelectorAll(".featuredMovies>.movieContainer>.affiche>.afficheFooter>.title")
+		val = document.querySelectorAll(".featuredMovies>.movieContainer>.affiche>div>.afficheFooter>.title")
 		val.forEach(elem => {
 			newList.push(elem.innerHTML)
 		});
@@ -71,6 +60,7 @@ function affMore() {
 
 		})
 		console.log(element)
+		
 		document.querySelector(".featuredMovies>.movieContainer").innerHTML += afficheMaker(element, valTaille)
 
 		if (val.length + 1 == array.length) {
@@ -78,11 +68,17 @@ function affMore() {
 			document.querySelector(".featuredMovies>div>.more-movie").setAttribute("onClick", "affReduce()")
 
 		}
-		console.log(document.querySelector(".featuredMovies>div>.more-movie").innerHTML)
-		console.log(val.length)
-		console.log(array.length)
-
+	}// end for
+}
+// function pour afficher moins de résultat
+function affReduce() {
+	document.querySelector(".featuredMovies>.movieContainer").innerHTML = ""
+	for (let i = 0; i < 6; i++) {
+		element = array[i]
+		document.querySelector(".featuredMovies>.movieContainer").innerHTML += afficheMaker(element, valTaille)
 	}
+	document.querySelector(".featuredMovies>div>.more-movie").innerHTML = "PLUS DE FILMS"
+	document.querySelector(".featuredMovies>div>.more-movie").setAttribute("onClick", "affMore()")
 }
 // affiche constructeur JS 
 function afficheMaker(object, taille) {
@@ -100,6 +96,7 @@ function afficheMaker(object, taille) {
 				  `
 
 }
+// function pour construction des modals 
 function modalMaker() {
 	`
 	<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
@@ -145,32 +142,46 @@ function modalMaker() {
 
 // function pour afficher les films avec flèches suivant précédent
 i = 0;
+// film suivant
 function nextMovie() {
 	console.log(i)
+	const iframe = document.querySelector(".moviePlay>.movie>.youtubeMovie>iframe");
+	const title = document.querySelector(".moviePlay>.infoMovie>ul>li.title");
+	const story = document.querySelector(".moviePlay>.infoMovie>ul>li>p.story");
+	const date = document.querySelector(".moviePlay>.infoMovie>ul>li>p.date");
+	const genre = document.querySelector(".moviePlay>.infoMovie>ul>li>p.genre");
+	const price = document.querySelector(".moviePlay>.infoMovie>ul>li>p.price");
 	if(i == array.length-1){
-
+		i = 0;
 	} else {
 		i++;
-		document.querySelector(".moviePlay>.movie>.youtubeMovie>iframe").src = `${array[i].ytsrc}`
-		document.querySelector(".moviePlay>.infoMovie>ul>li.title").innerHTML = `${array[i].title}`
-		document.querySelector(".moviePlay>.infoMovie>ul>li>p.story").innerHTML = `${array[i].story.slice(0, 100)}...`
-		document.querySelector(".moviePlay>.infoMovie>ul>li>p.date").innerHTML = `${array[i].date}`
-		document.querySelector(".moviePlay>.infoMovie>ul>li>p.genre").innerHTML = `${array[i].genre}`
-		document.querySelector(".moviePlay>.infoMovie>ul>li>p.price").innerHTML = `${array[i].price}`
+		iframe.src = `${array[i].ytsrc}`
+		title.innerHTML = `${array[i].title}`
+		story.innerHTML = `${array[i].story.slice(0, 100)}...`
+		date.innerHTML = `${array[i].date}`
+		genre.innerHTML = `${array[i].genre}`
+		price.innerHTML = `${array[i].price}`
 	}
 }
-
+// film précédent
 function beforeMovie() {
 	console.log(i)
+	const iframe = document.querySelector(".moviePlay>.movie>.youtubeMovie>iframe");
+	const title = document.querySelector(".moviePlay>.infoMovie>ul>li.title");
+	const story = document.querySelector(".moviePlay>.infoMovie>ul>li>p.story");
+	const date = document.querySelector(".moviePlay>.infoMovie>ul>li>p.date");
+	const genre = document.querySelector(".moviePlay>.infoMovie>ul>li>p.genre");
+	const price = document.querySelector(".moviePlay>.infoMovie>ul>li>p.price");
 	if(i == array.length || i == 0){
 		i = 0;
 	} else {
 		i--;
-		document.querySelector(".moviePlay>.movie>.youtubeMovie>iframe").src = `${array[i].ytsrc}`
-		document.querySelector(".moviePlay>.infoMovie>ul>li.title").innerHTML = `${array[i].title}`
-		document.querySelector(".moviePlay>.infoMovie>ul>li>p.story").innerHTML = `${array[i].story.slice(0, 100)}...`
-		document.querySelector(".moviePlay>.infoMovie>ul>li>p.date").innerHTML = `${array[i].date}`
-		document.querySelector(".moviePlay>.infoMovie>ul>li>p.genre").innerHTML = `${array[i].genre}`
-		document.querySelector(".moviePlay>.infoMovie>ul>li>p.price").innerHTML = `${array[i].price}`
+		iframe.src = `${array[i].ytsrc}`
+		title.innerHTML = `${array[i].title}`
+		story.innerHTML = `${array[i].story.slice(0, 100)}...`
+		date.innerHTML = `${array[i].date}`
+		genre.innerHTML = `${array[i].genre}`
+		price.innerHTML = `${array[i].price}`
 	}
 }
+
