fab99da04a159f8ef7f7bf8711bd4202ae7db567
BailleuxThomas <thomasbailleux2@gmail.com>
2018-11-13 16:35:46 +0100

    mise à jour

---
+++ assets/js/active.js
@@ -0,0 +1,14 @@
+$('#all-film, #action-film, #comeds-film').click(function() {
+    $('.btn-film').removeClass(' active');
+    $(this).addClass(' active');
+  });
+  
+  // $('#action-film').click(function() {
+  //   $('.btn-film').removeClass(' active');
+  //   $(this).addClass(' active');
+  // });
+  
+  // $('#comeds-film').click(function() {
+  //   $('.btn-film').removeClass(' active');
+  //   $(this).addClass(' active');
+  // });
